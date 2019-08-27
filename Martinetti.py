4830282cf22f00ad7f0a8238dcea7c99d216ee9b
SMDUNCAN208 <54329906+SMDUNCAN208@users.noreply.github.com>
2019-08-27 11:42:19 -0700

    Add files via upload

--- Martinetti.py
+++ Martinetti.py
@@ -1,93 +1,89 @@
 import random
 class Die :
-    def __init__(self, dieSides = 6) :
-        self.dieNum = 0
+    def __init__(self, _dieSides) :
+        numSides = _dieSides
 
     def __str__ ( self ) :
         return dieNum
 
-    def random ( self ) :
+    def roll ( self ) :
         dieNum = random.randint(1,6)
 
 
-class Testing :
-    def __init__(self)
-    dieFirst = Die() #First die
-    dieSecond = Die() #Second die
-    playerOne = ("") #Add name
-    playerTwo = ("") 
-    playerThree = ("")
-    test = Testing(Martinetti
-
-#test = Testing(Die())
-
-
 class Player :
-    def __init__(self, playerName) :
-        self.name = playerName
-
+    def __init__ (self, _name) :
+        name = _name
+        gameWon = 0
+        indexOne = 0
+        indexTwelve = 0
 
 class Martinetti :
-    def __init__ (self, dieFirst, dieSecond, playerNum)
-        self.dieOne = dieFirst 
-        self.dieTwo = dieSecond
-        self.positionOne = [1,2,3,4,5,6,7,8,9,10,11,12] #Marks position going up
-        self.positionTwo = [12,11,10,9,8,7,6,5,4,3,2,1] #Marks position going down
-        self.playerNum = 2
-        self.oneEmpty = false #true if the first array is all 0
-        self.twoEmpty = false #true if the second array is all 0
-        #0 will mean that a position has been reached
+    def __init__ (self, _dieOne, _dieTwo, _playerOne, _playerTwo) : 
+        dieOne = _dieOne
+        dieTwo = _dieTwo
+        dieThree = _dieThree
+        players = [_playerOne, _playerTwo]
+        one = [1,2,3,4,5,6,7,8,9,10,11,12]
+        twelve = [12,11,10,9,8,7,6,5,4,3,2,1]
+        numPlayers = 2
 
-    #Rolls the dice
-    def roll (self) 
-        dieOne.random()
-        dieTwo.random()
+    def simulateGame (self) :
+        gameOver = 1
 
-    # Checks and changes the position in the game (what numbers are crossed off)
-    def position (self)
-        roll()
-        oneEmpty = false
-        twoEmpty = false
-        numOne = 0 #number crossed off going up
-        numTwo = 0 #number crossed off going down
+        while gameOver == 1 :
+            end = false
+            doneOne = false
+            doneTwelve = false
 
-        for i in xrange(0,11, +1) :
-            if positionOne[i] == 0 :
-                numOne +
+            i = 0
 
-        if numOne == 12 :
-            for i in xrange(0,11, +1) :
-                if positionTwo[i] == 0 :
-                    numTwo +
+            while end == false :
+                indexesOne = [0,0]
+                indexesTwelve = [0,0]
+                dieFirst = dieOne.roll()
+                dieSecond = dieTwo.roll()
+                dieThird = dieThree.roll()
 
-        if numOne != 12 :
-            return numOne #returns position going up
+                print(players[i].name + " rolled a " + dieFirst + ", " + dieSecond + " , and a " + dieThird)
 
-        if numTwo != 12 :
-            return numTwo #returns position going down
+            if indexesOne[i] < 12 : 
+                while doneOne == false :
+                        if one[indexesOne[i]] == dieOne or one[indexesOne[i]] == dieTwo or one[indexesOne[i]] == dieThree or one[indexesOne[i]] == dieOne + dieTwo or one[indexesOne[i]] == dieOne + dieThree or one[indexesOne[i]] == dieTwo + dieThree or one[indexesOne[i]] == dieOne + dieTwo + dieThree :
+                            print(players[i].name + " hase crossed off " + one[indexesOne[i]])
+                            indexesOne[i] += 1
+                            if indexesOne[i] == 12 :
+                                doneOne = doneOne + 1
+                        else :
+                            doneOne = true
+                            print (players[i].name + "'s turn is over")
+            elif indexesTwelve[i] < 12 and indexesOne[i] == 12 :
+                while doneTwelve == false:
+                    if twelve[indexesTwelve[i]] == dieOne or twelve[indexesTwelve[i]] == dieTwo or twelve[indexesTwelve[i]] == dieThree or twelve[indexesTwelve[i]] == dieOne + dieTwo or twelve [indexesTwelve[i]] == dieOne + dieThree or twelve [indexesTwelve[i]] == dieTwo + dieThree or twelve[indexesTwelve[i]] == dieOne + dieTwo + dieThree :
+                            print(players[i].name + " has crossed off " + twelve[indexesTwelve[i]])
+                            twelve[i] += 1
+                            indexesTwelve[i] += 1
 
-        return 0 #no more positions
-        
+                            if indexesTwelve[i] == 12 :
+                                doneTwelve = true
 
-    def roll (self)
-        dieOne.random()
-        dieTwo.random()
-        calculate(position())
+                    else :
+                        doneTwele = true
+                        print (players[i].name + "'s turn is over")
+                            
+                    if indexesTwelve[i] == 12 :
+                        players[i].gamesWon += 1
+                        end = true
+                        print(player[i].name + " has won the game!")
+                        
+                      
 
-    def calculate (placement)
-        numOne = dieOne.number() #Die one roll
-        numTwo = dieTwo.number() #Die two roll
-
-        if placement == 0 :
-            #make a winning method
-
-        i = placement
-        while i != 0 :
-            if positionOne[placement] == numOne :
-                positionOne[placement] = 0
-            elif positionOne[placement] == numTwo :
-                positionOne[placement] = 0
-            elif positionOne[placement == numOne + numTwo :
-                positionOne[placement] = 0
-            else :
-                i = 0
+                             
+class Tests : 
+    def __init__( self ) :
+        dieFirst = Die() #First die
+        dieSecond = Die() #Second die
+        dieThird = Die() # Third die
+        cecilia = Player(Cecilia)
+        eva = Player (Eva)
+        test = Martinetti(dieFirst, dieSecond, dieThird, cecilia, eva)
+        test.simulateGame()
